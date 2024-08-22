c9040e1aecb0954a43209f27220889fba4b14882
DTiourine <109561478+DTiourine@users.noreply.github.com>
2024-08-21 20:24:28 -0400

    Modify API to return batch of images instead of just one

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
+import base64
+
 from fastapi import FastAPI, Response
 
-from utils import load_generator, generate_noise_vector, generate_image_tensor_from_noise_vector, tensor_to_webp
+from utils import load_generator, generate_noise_vector, generate_image_tensor_from_noise_vector, batch_tensor_to_webp
 from generator_architecture.architecture import Generator
 
 app = FastAPI()
@@ -15,6 +17,6 @@ def get_generator():
 async def root():
     generator = get_generator()
     noise_vector = generate_noise_vector()
-    image_tensor = generate_image_tensor_from_noise_vector(noise_vector, generator)
-    image = tensor_to_webp(image_tensor)
-    return Response(content=image, media_type="image/webp")
+    image_tensors = generate_image_tensor_from_noise_vector(noise_vector, generator)
+    images = batch_tensor_to_webp(image_tensors)
+    return [base64.b64encode(img).decode('utf-8') for img in images]
