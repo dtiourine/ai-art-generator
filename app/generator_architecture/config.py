fede6428f9c66863b0df1f17c621474af648d729
DTiourine <109561478+DTiourine@users.noreply.github.com>
2025-04-16 19:29:28 -0400

    Modifies GAN architecture

--- app/generator_architecture/config.py
+++
@@ -1,3 +0,0 @@
-NZ = 100  # Dimension of noise vector z
-NGF = 64  # Depth of generator
-NC = 3  # Number of color channels
--- src/train/train.py
+++ src/train/train.py
@@ -10,20 +10,8 @@ app = typer.Typer()
 
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+def main():
+    pass
 
 
 if __name__ == "__main__":
