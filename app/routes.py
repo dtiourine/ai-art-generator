1de6e769d6333e1b1f7282a719e7f6f980849bcf
dtiourine <109561478+DTiourine@users.noreply.github.com>
2025-04-16 23:14:46 -0400

    Adds dummy base64 image files and dummy endpoint to test how real images display on frontend

--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,10 @@ async def generate_art():
     image_tensor = generate_image_tensor_from_noise_vector(noise_vector, app_state.generator)
     base64_image = tensor_to_base64(image_tensor)
     return {"image": base64_image}
+
+
+# @router.post("/generate-art")
+# async def generate_art():
+#     with open('app/sample_image.txt', 'r') as f:
+#         base64_image = f.read()
+#     return {"image": base64_image}
